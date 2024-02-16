7f09e00f90563f3f66e4faf9f112ddc1fb130dc3
Jadap5 <160266446+Jadap5@users.noreply.github.com>
2024-02-16 12:31:05 -0500

    Add files via upload
    First submission

---
+++ P1LAB1_ParkerJada.py
@@ -0,0 +1,8 @@
+# Jada Parker
+# February 15, 2024
+# P1LAB1
+# This Python program demonstrates how to get input from the user and display output using IDLE.
+
+first_name = input("Hey Jada: ")
+
+print("Welcome to zyBooks!")
